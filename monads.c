e6ef4baeb6b18e226587727ce03bf8b40f2d1329
James R. <70611773+H-Art-Src@users.noreply.github.com>
2025-07-24 19:57:25 -0400

    Update documentation.

--- monads.c
+++ monads.c
@@ -23,11 +23,14 @@ Click any object/connection to select it.
 -If you're selecting an object currently at the current depth, right clicking another object at the same depth will create a one-way connection travelling to the right-clicked object.
 -It will instead create a one way connection from the right-clicked object if it is in a different category (container object).
 -You can change a link's travelling to object if you are selecting it and its containing object (usually will also be selected by clicking the link) by right clicking an object at the same depth.
+-If you hold the left mouse button down, you can drag the currently selected object around.
 
 -Key 'B' to delete all connections from and to a selected object.
 -Key 'Delete' to delete a selected object and recursively delete all objects contained within that object (and so on) and their connections from and to.
 If you are selecting a link it will delete that instead of an object.
--Key 'V' will rename the selected object to your clipboard contents.
+-Key 'T' will rename the selected object to your clipboard contents.
+-Key 'C' will copy the selected object and recursively for its sub-objects as text data into your clipboard.
+-Key 'V' will paste the clipboard contents, assuming it's good text data, recursively into named sub-objects of the selected object. This renames the selected object as well.
 */
 
 #include "raylib.h"
